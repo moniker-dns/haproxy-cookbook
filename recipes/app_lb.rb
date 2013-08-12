e964fc1de395feba982fd434e8f91295943aba00
Simon McCartney <simon.mccartney@hp.com>
2013-08-12 10:55:37 +0100

    replace app_lb discovery with search_helper_best_ip

--- recipes/app_lb.rb
+++ recipes/app_lb.rb
@@ -17,7 +17,6 @@
 # limitations under the License.
 #
 Chef::Log.info "haproxy::app_lb about to search.. role:#{node['haproxy']['app_server_role']} AND chef_environment:#{node.chef_environment}"
-pool_members = search("node", "role:#{node['haproxy']['app_server_role']} AND chef_environment:#{node.chef_environment}") || []
 Chef::Log.info "haproxy::app_lb found #{pool_members.count()} pool members"
 
 # load balancer may be in the pool
@@ -25,27 +24,9 @@ pool_members << node if node.run_list.roles.include?(node['haproxy']['app_server
 
 Chef::Log.info "haproxy::app_lb found #{pool_members.count()} pool members"
 
-# we prefer connecting via local_ipv4 if
-# pool members are in the same cloud
-# TODO refactor this logic into library...see COOK-494
-pool_members.map! do |member|
-  Chef::Log.info "haproxy::app_lb in the map...#{node['hostname']}"
-  server_ip = begin
-    if member.attribute?('meta_data')
-      Chef::Log.info "we #{node['hostname']} are in #{node['meta_data']['region']}"
-      Chef::Log.info "potential pool member #{member['hostname']} is in #{member['meta_data']['region']}"
-      if node.attribute?('meta_data') && (member['meta_data']['region'] == node['meta_data']['region'])
-        Chef::Log.info "using private_ipv4 #{member['meta_data']['private_ipv4']} for the pool_member"
-        member['meta_data']['private_ipv4']
-      else
-        Chef::Log.info "using public_ipv4 #{member['meta_data']['public_ipv4']} for the pool_member"
-        member['meta_data']['public_ipv4']
-      end
-    else
-      member['ipaddress']
-    end
-  end
-  {:ipaddress => server_ip, :hostname => member['hostname']}
+# we prefer connecting via local_ipv4 if # pool members are in the same cloud
+pool_members = search_helper_best_ip("node['haproxy']['app_server_role']} AND chef_environment:#{node.chef_environment}", node['haproxy'][app_servers]) do |ip, other_node|
+  {:ipaddress => ip, :hostname => other_node['hostname']}
 end
 
 package "haproxy" do
